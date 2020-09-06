c2bd12b0bae9fc5ac9f05eb93e0896500c67dae9
XU Bin <BXU1@dsone.3ds.com>
2020-09-06 11:01:14 +0200

    work on user loggout

---
+++ xbShopAdmin/src/pages/Dashboard/MainHeader/UserCenterMenu/userCenterMenu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Layout } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+// import LanguageMenu from '../../../components/Common/LanguageMenu/languageMenu';
+// import NotificationMenu from '../../../components/Common/Notification/notification';
+
+// import * as MetaActionCreator from '../../../store/action/metaActions';
+
+// import './main-header.scss';
+
+const UserCenter = () => {
+   return (
+    //    <div className="user-container">
+
+    //    </div>
+   )
+};
+
+export default UserCenter;
