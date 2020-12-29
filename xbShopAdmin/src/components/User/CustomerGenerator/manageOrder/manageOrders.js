e7368ffbe8f0a9a2b9f186b08fd32b4603dae7db
paker1989 <binxuqz@gmail.com>
2020-12-29 21:50:05 +0100

    commit

--- xbShopAdmin/src/components/User/CustomerGenerator/manageOrder/manageOrders.js
+++ xbShopAdmin/src/components/User/CustomerGenerator/manageOrder/manageOrders.js
@@ -5,13 +5,13 @@ import DashboardCard from '../../../Common/DashboardCard/DashboardCard';
 
 const ManageOrders = () => {
     return (
-        // <div className="section-container">
-        <Row>
-            <Col>
-                <DashboardCard title="No. of total commandes" value={4} />
-            </Col>
-        </Row>
-        /* </div> */
+        <div className="section-container">
+            <Row>
+                <Col>
+                    <DashboardCard title="No. of total commandes" value={4} />
+                </Col>
+            </Row>
+        </div>
     );
 };
 
